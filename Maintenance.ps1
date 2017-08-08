f7c8d1f7fbed59f782c3e0919dce28be395f3ef2
TheAlabaster92 <thealabaster92@hotmail.com>
2017-08-08 22:25:24 +0200

    v0.2.3.20170808.1704

--- Maintenance.ps1
+++ Maintenance.ps1
@@ -1,70 +1,18 @@
 <#
     Created by Antonio Martino <TheAlabaster92>, March 16th 2017, copyright© 2017 - All rights reserved.
-    check the EULA on http://thealabaster92.sharkignite-studios.com/eula/ for more info.
-
-    Name:			AltMaint;
-    Date:			March 16th 2017;
-    Version:		v0.2.2;
-    Dev_Number:		20170803.2202;
-
-    Description:
-      Automated maintenance script with log files and final loop to auto shutdown.
-      This script contains some basic maintenance tasks that have to be performed every week, it allows
-      for an easy set and forget maintenance.
-      
-      You can start this script by opening a powershell window as admin and going to the directory where this script is saved,
-      then run the command .\<script name> where <script name> is the actual file name.
-
-      Note:
-        1. There's to note that this script will only use chkdsk to check the disk for errors, no fix will be performed.
-           If and when a fix will be needed, chkdsk should be run manually by the user.
-           Please check the logs after each maintenance to check if you need to fix errors on disk.
-        2. This script makes use of third parties tools like Ccleaner and Auslogics Disk Defrag.
-        3. Some options have to be setup before the script is run. please refer to the documentation for cleanmgr /sageset
-           and ccleaner /auto commands.
-		4. Chkdsk has been updated to fetch all the drives present on the computer, it will use a Get-PSDrive to list every
-		   Virtual Drive.
-		   If you're unsure the drive can be checked with ChkDsk or simply don't want to scan it, remove the drive before
-		   running the script.
-
-    Changelog:
-	  v0.2.2: Added Names for Jobs;
-			  (Experimental) - Fixed code to support local tools, removing the necessity to set Path variable;
-			  Reviewed version number following the actual bersioning convention;
-      v0.2.1: (Experimental) - Added CCleaner and Aus. Disk Defrag to script folder;
-              Fixed an issue with Chkdsk and Defrag loop that wouldn't let the tools run;
-              Removed old code;
-	  v0.2.0: Added comments to each part of the script;
-			  Added foreach loop for chkdsk to avoid entering all drives letters;
-			  Added a note for ChkDsk command;
-			  Removed duplicated tasks and un-necessary operations;
-			  Removed Defraggler tool;
-			  Changed paths to use the local variable $logPath;
-			  Moved all the comments to the end of the script;
-			  Increased wait time after maintenance from 5 to 15 minutes before shutting down;
-			  Increased the scheduled time for shutdown from 30 to 60 seconds;
-			  Fixed check for logPath existance;
-			  Fixed file output;
-			  Fixed console output;
-      v0.1.4: Added auslogics disk defrag tool since defragging with defraggler took a lot of time;			  
-              Switched from defraggler 64 to defraggler 32 bit;
-              Fixed some text errors;
-      v0.1.3: Fixed console output format;
-      v0.1.2: Added regular expression to format the output of sfc /scannow command;
-              Added copy of the advanced log files from windows for SFC and DISM;
-              Added more maintenance scans;
-              Added messages before the start of each procedure;
-	  v0.1.1: Fixed a typo that prevented Dism tool from running;
-      v0.1.0: Added final Loop to auto shutdown after 5 minutes if the user is not around;
-      v0.0.7: Changed .txt files to .log files for logging purposes;
-      v0.0.5: Removed echo on each line and left only the jobs output;
-      v0.0.2: Fixed some issues where jobs would be skipped even if Wait-Job was issued;
-      v0.0.1: First version, was a conversion from a batch script to powershell;
+	Check the EULA on https://alacrisys.com/EULA for more info.
+	
 #>
 
 #Preliminary Operations--------------------------------------------------------------------------------------
+#Fields:
 $logPath = "C:\Maint-Log\";
 $logFolderExists = Test-Path $logPath
+$ccleanerPath = Resolve-Path .\CCleaner\ccleaner64.exe;
+$ccleanerPath = $ccleanerPath.Path;
+$ausDDPath = Resolve-Path .\AusDiskDefrag\cdefrag.exe;
+$ausDDPath = $ausDDPath.Path;
+
 if ($logFolderExists -eq $false) {
   Write-Host "Log folder doesn't exists, making a new one...";
   mkdir $logPath;
@@ -83,24 +31,24 @@ Write-Host " ";
 
 
 #Cleanup-----------------------------------------------------------------------------------------------------
-Write-Host "Pre-cleanup with windows clean manager...";
+Write-Host -NoNewline "Pre-cleanup with windows clean manager...";
 Start-Process cleanmgr /sagerun:1 -Wait;
 Write-Host "Done.`n";
 
-Write-Host "Pre-cleanup with ccleaner...";
-Start-Process ccleaner64.lnk /Auto -Wait;
+Write-Host -NoNewline "Pre-cleanup with ccleaner...";
+Start-Process $ccleanerPath /Auto -Wait;
 Write-Host "Done.`n";
 
-Write-Host "File cleanup with windows clean manager...";
+Write-Host -NoNewline "File cleanup with windows clean manager...";
 Start-Process cleanmgr /sagerun:1 -Wait;
 Write-Host "Done.`n";
 
-Write-Host "File cleanup with ccleaner...";
-Start-Process ccleaner64.lnk /Auto -Wait;
+Write-Host -NoNewline "File cleanup with ccleaner...";
+Start-Process $ccleanerPath /Auto -Wait;
 Write-Host "Done.`n";
 
 
-#Maintenance-------------------------------------------------------------------------------------------------
+#Maintenance Queue Message-----------------------------------------------------------------------------------
 Write-Host "Queued jobs: 
 1. SFC /ScanNow,
 2. DISM /Online /Cleanup-Image /StartComponentCleanup,
@@ -164,9 +112,11 @@ foreach ($drive in Get-PSDrive -PSProvider 'FileSystem') {
 #Defrag with Auslogics Disk Defrag (all drives)--------------------------------------------------------------
 #Write-Host -NoNewline "Running Defrag with Auslogics Disk Defrag (all drives)...";
 Write-Output "-------------------$date-------------------" >> $logPath"Auslogics_Disk_Defrag.log";
-$job = Start-Job {cdefrag.lnk -dt -o -c} -Name "Aus. DD all drives";
+$par = "-dt -o -c";
+$job = Start-Job -ScriptBlock {Start-Process -FilePath $args[0] -ArgumentList $args[1] -Wait;} -ArgumentList $ausDDPath, $par -Name "Aus. DD all drives";
 Wait-Job $job;
 Receive-Job $job | Out-File $logPath"Auslogics_Disk_Defrag.log" -Append;
+(Get-Content $logPath"Auslogics_Disk_Defrag.log") -replace "\x00", "" | Set-Content $logPath"Auslogics_Disk_Defrag.log";
 (Get-Content $logPath"Auslogics_Disk_Defrag.log") -replace "[\x08]+", "`n" | Set-Content $logPath"Auslogics_Disk_Defrag.log";
 
 #SFC Third Pass----------------------------------------------------------------------------------------------
--- ReadMe.txt
+++ ReadMe.txt
@@ -1,13 +1,35 @@
 Project Info:
 
 Name            AltMaint
-Description		Alternative maintenance script to windows automatic maintenance.
-Notes			It must be run as Admin to work properly.
-Version         0.2.2.20170803.2202
+Extended Name   Alternative windows maintenance
+Version         v0.2.3.20170808.1704
 Author          TheAlabaster92
+Copyright       Created by Antonio Martino <TheAlabaster92>, March 16th 2017, copyright© 2017 - All rights reserved.
+EULA            https://alacrisys.com/EULA
 
 
 Contact Info:
 
-Website:		https://alacrisys.com
-Contacts:		thealabaster92@hotmail.com
+Website:        https://alacrisys.com
+Contacts:       thealabaster92@hotmail.com
+
+
+Description:
+  Automated maintenance script with log files and final loop to auto shutdown.
+  This script contains some basic maintenance tasks that have to be performed every week, it allows
+  for an easy set and forget maintenance.
+  
+  You can start this script by opening a powershell window as admin and going to the directory where this script is saved,
+  then run the command .\<script name> where <script name> is the actual file name.
+
+  Note:
+	1. There's to note that this script will only use chkdsk to check the disk for errors, no fix will be performed.
+	   If and when a fix will be needed, chkdsk should be run manually by the user.
+	   Please check the logs after each maintenance to check if you need to fix errors on disk.
+	2. This script makes use of third parties tools like Ccleaner and Auslogics Disk Defrag.
+	3. Some options have to be setup before the script is run. please refer to the documentation for cleanmgr /sageset
+	   and ccleaner /auto commands.
+	4. Chkdsk has been updated to fetch all the drives present on the computer, it will use a Get-PSDrive to list every
+	   Virtual Drive.
+	   If you're unsure the drive can be checked with ChkDsk or simply don't want to scan it, remove the drive before
+	   running the script.
